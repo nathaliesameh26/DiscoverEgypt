bd1918f8b2370cf113fc1a47fc46944ac759c036
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2022-10-29 20:25:27 +0200

    the model of the places page

---
+++ mobileapplication/lib/places_model.dart
@@ -0,0 +1,10 @@
+//here we will write the places data model class
+
+class PlacesModel {
+  String? name;
+  String? location;
+  String? description;
+  int? price;
+
+  PlacesModel(this.name, this.location, this.description, this.price);
+}

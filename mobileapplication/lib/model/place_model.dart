82528149753bde68f36fd1872a79b2f2cb05877c
Remon74 <remon1908077@miuegypt.edu.eg>
2022-10-30 22:16:43 +0200

    place_model

--- mobileapplication/lib/model/place_model.dart
+++ mobileapplication/lib/model/place_model.dart
@@ -1,6 +1,5 @@
 class PlaceInfo {
-  final String location, image, name, desc;
-  final int time, price;
+  final String location, image, name, desc, time, price;
 
   PlaceInfo({
     required this.name,
@@ -16,41 +15,33 @@ List places = [
   PlaceInfo(
       image: 'assets/kyaking.jpg',
       location: 'zamalek',
-      time: 9,
+      time: "9 am",
       name: 'Kyaking',
       desc:
           'This is a fun activity that involves moving through water in a small water vessel with the aid of a double-bladed paddle',
-      price: 250),
+      price: "100 Dollars"),
   PlaceInfo(
       image: 'assets/kamel.jpg',
       location: 'Giza',
-      time: 1,
-      name: 'Riding kamels&hourses',
+      time: "1 AM",
+      name: 'Riding Camels&hourses',
       desc:
           'Enjoy a camel ride at Giza Pyramids for 2 hours during the sun rise or sun set. Mount a camel and ride it through the desert to the base of the Great Pyramid.',
-      price: 150),
-  PlaceInfo(
-      image: 'assets/kamel.jpg',
-      location: 'Giza',
-      time: 1,
-      name: 'Riding kamels&hourses',
-      desc:
-          'Enjoy a camel ride at Giza Pyramids for 2 hours during the sun rise or sun set. Mount a camel and ride it through the desert to the base of the Great Pyramid.',
-      price: 150),
+      price: "50 Dollars"),
   PlaceInfo(
       image: 'assets/luxor.jpg',
       location: 'luxor',
-      time: 1,
+      time: "1AM",
       name: 'luxor temple',
       desc:
           ' one of the oldest and most inhabited cities on earth, the capital of the pharaohs in Ancient Egypt.',
-      price: 1500),
+      price: "1000 Dollars"),
   PlaceInfo(
       image: 'assets/pyramids.jpg',
       location: 'Giza',
-      time: 1,
+      time: "9Am",
       name: 'pyramids',
       desc:
           '  the worlds oldest monumental structures constructed of dressed masonry',
-      price: 200)
+      price: "200 Dollars")
 ];

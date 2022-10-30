da7f0a334a2b2be8d0c95993e921e2ee75aa680e
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-10-31 01:40:03 +0200

    admin1

---
+++ mobileapplication/lib/adminpanel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Admin extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return adminpanelState();
+  }
+}
+
+class adminpanelState extends State<Admin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}

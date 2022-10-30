9eeb19b9826d7f6ff416a1ab7bcb706d85899559
Amir1912710 <79427346+Amir1912710@users.noreply.github.com>
2022-10-31 01:11:09 +0200

    update home screen

--- mobileapplication/lib/screens/home/home_screen.dart
+++ mobileapplication/lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapplication/model/place_model.dart';
+import 'package:mobileapplication/widget/navigation_bar.dart';
 
 import '../../widget/category_card.dart';
 import '../../widget/category_card.dart';
@@ -166,7 +167,7 @@ class RecommendedCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      elevation: 5,
+      //elevation: 5,
       child: Container(
         height: 210,
         width: 200,
@@ -176,7 +177,7 @@ class RecommendedCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              height: 150,
+              height: 100,
               // width: double.infinity,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
@@ -206,6 +207,115 @@ class RecommendedCard extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MyHomePage> {
+  int index = 0;
+  final screens = [
+    Center(
+      child: Text(
+        'Page 1',
+        style: TextStyle(fontSize: 72),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Page 2',
+        style: TextStyle(fontSize: 72),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Page 3',
+        style: TextStyle(fontSize: 72),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Page 4',
+        style: TextStyle(fontSize: 72),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Page 5',
+        style: TextStyle(fontSize: 72),
+      ),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    //final backgroundimage =
+    //'https://w0.peakpx.com/wallpaper/574/129/HD-wallpaper-egypt-pyramid-sunset.jpg';
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("EgyMania"),
+      ),
+      endDrawer: Drawer(
+          child: ListView(
+        children: [
+          DrawerHeader(
+            child: Text('Drawer Header'),
+            decoration: BoxDecoration(color: Colors.orange),
+          ),
+          ListTile(title: Text('item1')),
+          ListTile(title: Text('item2')),
+        ],
+      )),
+      body: screens[index],
+      // body: Container(
+      //     decoration: BoxDecoration(
+      //         image: DecorationImage(
+      //   image: NetworkImage(backgroundimage),
+      //   fit: BoxFit.cover,
+
+      // ))),
+
+      bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+              indicatorColor: Colors.orange,
+              labelTextStyle: MaterialStateProperty.all(
+                TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              )),
+          child: NavigationBar(
+            height: 60,
+            backgroundColor: Colors.transparent,
+            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            selectedIndex: index,
+            onDestinationSelected: (index) =>
+                setState(() => this.index = index),
+            destinations: [
+              NavigationDestination(
+                  icon: Icon(Icons.tour_outlined),
+                  selectedIcon: Icon(Icons.tour_rounded),
+                  label: 'Explore'),
+              NavigationDestination(
+                  icon: Icon(Icons.event_available_outlined),
+                  selectedIcon: Icon(Icons.event_available_rounded),
+                  label: 'Events'),
+              NavigationDestination(
+                  icon: Icon(Icons.favorite_border_outlined),
+                  selectedIcon: Icon(Icons.favorite),
+                  label: 'Wish List'),
+              NavigationDestination(
+                  icon: Icon(Icons.map_outlined),
+                  selectedIcon: Icon(Icons.map),
+                  label: 'My Plans'),
+              NavigationDestination(
+                  icon: Icon(Icons.account_box_outlined),
+                  selectedIcon: Icon(Icons.account_box_rounded),
+                  label: 'Account'),
+            ],
+          )),
     );
   }
 }

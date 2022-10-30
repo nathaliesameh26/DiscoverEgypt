41cbe80186bc180892156685948b4244dc95129f
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2022-10-30 23:57:29 +0200

    update destination screen

--- mobileapplication/lib/destinantion_screen.dart
+++ mobileapplication/lib/destinantion_screen.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
-import 'destinantion_model.dart';
+import 'destination_model.dart';
 import 'activity_model.dart';
 
-
 class DestinationScreen extends StatefulWidget {
-  //final Destination destination;
+  final Destination destination;
 
-  //DestinationScreen({this.destination});
+  DestinationScreen({required this.destination});
 
   @override
   _DestinationScreenState createState() => _DestinationScreenState();
 }
 
 class _DestinationScreenState extends State<DestinationScreen> {
-
-
-   Text _buildRatingStars(int rating){
+  Text _buildRatingStars(int rating) {
     String stars = '';
-    for(int i =0;i<rating;i++){
+    for (int i = 0; i < rating; i++) {
       stars += 'star';
-
     }
     stars.trim();
     return Text(stars);
- 
-}
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -118,7 +112,7 @@ class _DestinationScreenState extends State<DestinationScreen> {
                           width: 5.0,
                         ),
                         Text(
-                          widget.destinantion.country,
+                          widget.destination.country,
                           // ignore: prefer_const_constructors
                           style: TextStyle(
                             color: Colors.white,
@@ -142,124 +136,122 @@ class _DestinationScreenState extends State<DestinationScreen> {
             ],
           ),
           Expanded(
-            child:  ListView.builder(
-              padding: EdgeInsets.only(top: 10.0, bottom: 15.0),
-              itemCount: widget.Destination.activities.length,
-              itemBuilder: (BuildContext (context, index) {
-                Activity activity = widget.destination.activities[index];
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0, bottom: 15.0),
+                itemCount: widget.destination.activities.length,
+                itemBuilder: (BuildContext context, index) {
+                  Activity activity = widget.destination.activities[index];
 
-                return Stack(
-                  children:<Widget> [
-                    Container(
-                      margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
-                      height: 170.0,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0)
-                      ),
-                      child: Padding(
-                        padding:  EdgeInsets.fromLTRB(100.0,20.0,20.0,20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children:<Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  return Stack(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+                        height: 170.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
-                               children:<Widget> [
-                            Container(
-                              width: 120.0,
-                            child: Text(activity.name, 
-                            style:TextStyle(
-                            fontSize:18.0 ,
-                            fontWeight: FontWeight.w600,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines:2 ,
-                            ),
-                            
-                            ),
-                            Column(
-                              children: [
-                                Text('\$${activity.price}',
-                                style: const TextStyle(
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w600,
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Container(
+                                      width: 120.0,
+                                      child: Text(
+                                        activity.name,
+                                        style: TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                      ),
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text(
+                                          '\$${activity.price}',
+                                          style: const TextStyle(
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        const Text(
+                                          'per pax',
+                                          style: TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
+                                Text(activity.type, style: TextStyle()),
+                                _buildRatingStars(activity.rating),
+                                SizedBox(
+                                  height: 10.0,
                                 ),
-                                const Text('per pax',
-                                
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                                ),
-                              ],
-                            ),
-                          ],
-                            ),
-                            Text(activity.type, style:TextStyle(
-
-                            )),
-                             _buildRatingStars(activity.rating),
-                            SizedBox(height: 10.0,),
-                            Row(
-                              children:<Widget> [
-                                Container(
-                                  padding: EdgeInsets.all(5.0),
-                                  width: 70.0,
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).accentColor,
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    activity.startTime[0],
-                                  ),
-                                ),
-                                SizedBox(height: 10.0,),
-                                 Container(
-                                  padding: EdgeInsets.all(5.0),
-                                  width: 70.0,
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).accentColor,
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    activity.startTime[1],
-                                  ),
+                                Row(
+                                  children: <Widget>[
+                                    Container(
+                                      padding: EdgeInsets.all(5.0),
+                                      width: 70.0,
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context).accentColor,
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        activity.startTimes[0],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(5.0),
+                                      width: 70.0,
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context).accentColor,
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        activity.startTimes[1],
+                                      ),
+                                    )
+                                  ],
                                 )
-                              ],
-                            )
-                          ]
-                         
+                              ]),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      left: 20.0,
-                      top: 15.0,
-                      bottom: 15.0,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.asset(
-                    'assets/Egypt.jpg',
-                    width: 110.0,
-                     fit: BoxFit.cover,
-                      ),
-                    )
-                    ),
-                  ],
-                );
-              }),
-
-            ),
+                      Positioned(
+                          left: 20.0,
+                          top: 15.0,
+                          bottom: 15.0,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image.asset(
+                              'assets/Egypt.jpg',
+                              width: 110.0,
+                              fit: BoxFit.cover,
+                            ),
+                          )),
+                    ],
+                  );
+                }),
           ),
-
-            
         ],
       ),
     );

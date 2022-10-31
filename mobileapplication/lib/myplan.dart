ee14ebd0164a619226b47bac606e0cbed949db96
Amir1912710 <79427346+Amir1912710@users.noreply.github.com>
2022-10-31 08:20:07 +0200

    updated myplan

--- mobileapplication/lib/myplan.dart
+++ mobileapplication/lib/myplan.dart
@@ -15,61 +15,18 @@ class MyPlans extends StatefulWidget {
 }
 
 class _MyPlanState extends State<MyPlans> {
-  int index = 0;
-  final screens = [
-    const HomeScreen(),
-    const Center(
-      child: Text(
-        'Page 2',
-        style: TextStyle(fontSize: 72),
-      ),
-    ),
-    const WishList(),
-    const Center(
-      child: Text(
-        'Page 4',
-        style: TextStyle(fontSize: 72),
-      ),
-    ),
-    const Center(
-      child: Text(
-        'Page 5',
-        style: TextStyle(fontSize: 72),
-      ),
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        //titleTextStyle: TextStyle(color: Colors.black),
-        title: const Text(
-          "EgyMania",
-          textAlign: TextAlign.right,
-        ),
-      ),
-      drawer: Drawer(
-          child: ListView(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          const DrawerHeader(
-            child: Text('Drawer Header'),
-            decoration: BoxDecoration(color: Colors.orange),
-          ),
-          const ListTile(title: Text('item1')),
-          const ListTile(title: Text('item2')),
-        ],
-      )),
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
         child: Column(children: [
           const SizedBox(
-            height: 15,
+            height: 80,
           ),
-          const Text("Your WishList",
+          const Text("My Plans",
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
           const SizedBox(
             height: 20,
@@ -103,42 +60,6 @@ class _MyPlanState extends State<MyPlans> {
           ),
         ]),
       )),
-      bottomNavigationBar: NavigationBarTheme(
-          data: NavigationBarThemeData(
-              indicatorColor: Colors.orange,
-              labelTextStyle: MaterialStateProperty.all(
-                TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-              )),
-          child: NavigationBar(
-            height: 60,
-            backgroundColor: Colors.transparent,
-            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-            selectedIndex: index,
-            onDestinationSelected: (index) =>
-                setState(() => this.index = index),
-            destinations: [
-              NavigationDestination(
-                  icon: Icon(Icons.tour_outlined),
-                  selectedIcon: Icon(Icons.tour_rounded),
-                  label: 'Explore'),
-              NavigationDestination(
-                  icon: Icon(Icons.event_available_outlined),
-                  selectedIcon: Icon(Icons.event_available_rounded),
-                  label: 'Events'),
-              NavigationDestination(
-                  icon: Icon(Icons.favorite_border_outlined),
-                  selectedIcon: Icon(Icons.favorite),
-                  label: 'Wish List'),
-              NavigationDestination(
-                  icon: Icon(Icons.map_outlined),
-                  selectedIcon: Icon(Icons.map),
-                  label: 'My Plans'),
-              NavigationDestination(
-                  icon: Icon(Icons.account_box_outlined),
-                  selectedIcon: Icon(Icons.account_box_rounded),
-                  label: 'Account'),
-            ],
-          )),
     );
   }
 }

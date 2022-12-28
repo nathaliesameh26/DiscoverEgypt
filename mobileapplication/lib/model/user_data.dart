752b005e33cf2da638955894577b77d970536831
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2022-12-29 01:17:26 +0200

    update the user data

---
+++ mobileapplication/lib/model/user_data.dart
@@ -0,0 +1,13 @@
+import 'package:mobileapplication/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath: 'https://cdn-icons-png.flaticon.com/512/146/146007.png',
+    name: 'EgyMania User',
+    email: 'EgyManiaUser@gmail.com',
+    password: '123456',
+    about:
+        'A MIU Computer science students works on EgyMania project acts as User in this application.',
+    isDarkMode: false,
+  );
+}

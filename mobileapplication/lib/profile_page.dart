a836cd2dede78d9a7697b02427f4e199f4d19d1e
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2022-12-29 01:17:59 +0200

    change profile page imports

--- mobileapplication/lib/profile_page.dart
+++ mobileapplication/lib/profile_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobileapplication/model/user.dart';
 import 'package:mobileapplication/edit_profile_page.dart';
-import 'package:mobileapplication/data/user_data.dart';
+import 'package:mobileapplication/model/user_data.dart';
 import 'package:mobileapplication/widget/profile_widget.dart';
 
 class ProfilePage extends StatefulWidget {

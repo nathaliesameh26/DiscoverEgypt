ea273203715886e8d31d40cf23891a32321107ce
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-12-30 03:26:27 +0200

    swift

--- mobileapplication/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobileapplication/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
-
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))

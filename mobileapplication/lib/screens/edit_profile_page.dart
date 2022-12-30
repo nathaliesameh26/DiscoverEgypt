45abbda994e794716b039e771bc3f86252d3934f
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-12-30 03:41:22 +0200

    edit

---
+++ mobileapplication/lib/screens/edit_profile_page.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:mobileapplication/model/user.dart';
+import '../model/user_model.dart';
+import 'package:mobileapplication/widget/profile_widget.dart';
+import 'package:mobileapplication/widget/textfield_widget.dart';
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  User user = UserPreferences.myUser;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        //appBar: buildAppBar(context),
+        body: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          physics: BouncingScrollPhysics(),
+          children: [
+            const SizedBox(height: 50),
+            ProfileWidget(
+              imagePath: user.imagePath,
+              isEdit: true,
+              onClicked: () async {},
+            ),
+            const SizedBox(height: 24),
+            TextFieldWidget(
+              label: 'Full Name',
+              text: user.name,
+              onChanged: (name) {},
+            ),
+            const SizedBox(height: 24),
+            TextFieldWidget(
+              label: 'Email',
+              text: user.email,
+              onChanged: (email) {},
+            ),
+            const SizedBox(height: 24),
+            TextFieldWidget(
+              label: 'Password',
+              text: user.password,
+              onChanged: (password) {},
+            ),
+            const SizedBox(height: 24),
+            TextFieldWidget(
+              label: 'About',
+              text: user.about,
+              maxLines: 5,
+              onChanged: (about) {},
+            ),
+          ],
+        ),
+      );
+}

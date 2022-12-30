10da6f791bd6d7f823f404db0d4ee206da07214e
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-12-30 03:24:02 +0200

    main

--- mobileapplication/lib/main.dart
+++ mobileapplication/lib/main.dart
@@ -39,6 +39,9 @@ class MyApp extends StatelessWidget {
           '/Searchplaces': (context) => const SearchPage(),
           '/admin': (context) => const adminpnel(),
           //'/evenetPlannerForm': (context) => Eventform(),
+
+          // '/homescreen': (context) => const HomeScreenn(),
+
           '/wishList': (context) => const WishList(),
           '/myplans': (context) => const MyPlans(),
           '/welcome': (context) => const Welcome(),

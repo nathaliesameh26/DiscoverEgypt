bce2d6fbaea848d1dea2268defe1f9135fa23ee7
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-12-30 03:25:58 +0200

    bg

--- mobileapplication/lib/model/user.dart
+++ mobileapplication/lib/model/user.dart
@@ -2,21 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserData {
-  // final String imagePath;
-  // final String name;
-  // final String email;
-  // final String password;
-  // final String about;
-  // final bool isDarkMode;
-
-  // const User({
-  //   required this.imagePath,
-  //   required this.name,
-  //   required this.email,
-  //   required this.password,
-  //   required this.about,
-  //   required this.isDarkMode,
-  // });
   Future<Object> getUserDetails() async {
     final user = FirebaseAuth.instance.currentUser!;
     String userIds = user.uid;

263684ed8272db57de75be52e17cacd701963bcd
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2022-12-31 21:01:07 +0200

    create the places provider file

---
+++ mobileapplication/lib/data/repositries/places_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../model/place_model.dart';
+import '../../model/user.dart';
+
+//provider byklm almodel
+Future placesData = PlacesData().PlaceDetails();
+final placesDataProviderRepository = StateProvider<Future>((ref) => placesData);
+//gets the data from the model
+
+final placesDataProvider = FutureProvider(
+  //bagyb beh aldata,byt3amel my al-ui
+  //ui byklem provider
+  (ref) async {
+    return ref.watch(placesDataProviderRepository);
+    //listens to the data
+  },
+);

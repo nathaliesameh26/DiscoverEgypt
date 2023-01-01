bea0ca27c5c89b675c8591ab8460b6d207fbac7e
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-01 03:26:34 +0200

    home

--- mobileapplication/lib/screens/homeScreen.dart
+++ mobileapplication/lib/screens/homeScreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mobileapplication/screens/events_carousel.dart';
 
+import '../widget/navigation_bar.dart';
 import 'destination_carousel.dart';
 
 class HomeScreenn extends StatefulWidget {
@@ -16,10 +17,10 @@ class HomeScreenn extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreenn> {
   int _selectedIndex = 0;
   final List<IconData> _icons = [
-    FontAwesomeIcons.plane,
-    FontAwesomeIcons.bed,
-    FontAwesomeIcons.plane,
-    FontAwesomeIcons.biking,
+    FontAwesomeIcons.houseMedical,
+    FontAwesomeIcons.eye,
+    FontAwesomeIcons.arrowsToEye,
+    FontAwesomeIcons.searchengin,
   ];
   Widget _buildIcon(int index) {
     return GestureDetector(
@@ -34,15 +35,15 @@ class _HomeScreenState extends State<HomeScreenn> {
           width: 60.0,
           decoration: BoxDecoration(
               color: _selectedIndex == index
-                  ? Theme.of(context).accentColor
-                  : Color(0xFFE7BEE),
+                  ? Theme.of(context).cardColor
+                  : Color.fromARGB(15, 196, 189, 196),
               borderRadius: BorderRadius.circular(30)),
           child: Icon(
             _icons[index],
             size: 25.0,
             color: _selectedIndex == index
                 ? Theme.of(context).primaryColor
-                : Color(0xFFBC1C4),
+                : Color.fromARGB(224, 0, 0, 0),
           ),
         ));
   }
@@ -51,40 +52,41 @@ class _HomeScreenState extends State<HomeScreenn> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView(
-        padding: EdgeInsets.symmetric(vertical: 30.0),
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 120.0),
-            child: Text(
-              'What would you like to find?',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text(
+                'What would you like to find?',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: _icons
-                .asMap()
-                .entries
-                .map((MapEntry map) => _buildIcon(map.key))
-                .toList(),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          DestinationCarousel(),
-          SizedBox(
-            height: 20.0,
-          ),
-          EventCarousel(),
-        ],
-      )),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map((MapEntry map) => _buildIcon(map.key))
+                  .toList(),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            DestinationCarousel(),
+            const SizedBox(
+              height: 20.0,
+            ),
+            EventCarousel(),
+          ],
+        ),
+      ),
     );
   }
 }

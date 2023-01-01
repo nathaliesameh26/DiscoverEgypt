9f153e796cbe507468c7a14c2bdbe0fa61ea32b2
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-01 03:27:05 +0200

    recom

--- mobileapplication/lib/widget/recommended_card.dart
+++ mobileapplication/lib/widget/recommended_card.dart
@@ -1,63 +1,63 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import '../model/place_model.dart';
+// import '../model/place_model.dart';
 
-class RecommendedCard extends StatelessWidget {
-  final PlaceInfo placeInfo;
-  final VoidCallback press;
-  const RecommendedCard({
-    Key? key,
-    required this.placeInfo,
-    required this.press,
-  }) : super(key: key);
+// class RecommendedCard extends StatelessWidget {
+//   //final PlaceInfo placeInfo;
+//   final VoidCallback press;
+//   const RecommendedCard({
+//     Key? key,
+//     //required this.placeInfo,
+//     required this.press,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: press,
-      child: Material(
-        elevation: 5,
-        child: Container(
-          height: 210,
-          width: 200,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(20)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 150,
-                // width: double.infinity,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                        fit: BoxFit.cover, image: AssetImage(placeInfo.image))),
-              ),
-              Text(
-                placeInfo.name,
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.location_on,
-                    color: Colors.orange,
-                  ),
-                  Text(placeInfo.location,
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold)),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: press,
+//       child: Material(
+//         elevation: 5,
+//         child: Container(
+//           height: 210,
+//           width: 200,
+//           decoration: BoxDecoration(
+//               color: Colors.white, borderRadius: BorderRadius.circular(20)),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Container(
+//                 height: 150,
+//                 // width: double.infinity,
+//                 decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(20),
+//                     image: DecorationImage(
+//                         fit: BoxFit.cover, image: AssetImage(placeInfo.image))),
+//               ),
+//               Text(
+//                 placeInfo.name,
+//                 style:
+//                     const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//               ),
+//               const SizedBox(
+//                 height: 3,
+//               ),
+//               Row(
+//                 children: [
+//                   const Icon(
+//                     Icons.location_on,
+//                     color: Colors.orange,
+//                   ),
+//                   Text(placeInfo.location,
+//                       style: const TextStyle(
+//                           color: Colors.black,
+//                           fontSize: 18,
+//                           fontWeight: FontWeight.bold)),
+//                 ],
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }

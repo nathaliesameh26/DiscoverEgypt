6e8a8c9e8b2acd4521ddb3806dbd5834b5dbb4d3
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-01-04 15:09:14 +0200

    update the splash screen

--- mobileapplication/lib/screens/splash.dart
+++ mobileapplication/lib/screens/splash.dart
@@ -23,7 +23,7 @@ class _SplashState extends State<Splash> {
   }
 
   route() {
-    Navigator.of(context).pushReplacementNamed('/homescreen');
+    Navigator.of(context).pushReplacementNamed('/Welcome');
   }
 
   @override

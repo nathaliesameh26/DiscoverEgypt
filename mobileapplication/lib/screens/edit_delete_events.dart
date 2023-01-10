90a1b638f671d522c30e42ddeae1a8fed571c17c
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-10 02:47:07 +0200

    crudevents

--- mobileapplication/lib/screens/edit_delete_events.dart
+++ mobileapplication/lib/screens/edit_delete_events.dart
@@ -1,21 +1,205 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../data/repo/events_provider.dart';
 
-class crudevents extends StatefulWidget {
-  const crudevents({super.key});
+class CrudEvent extends ConsumerStatefulWidget {
+  CrudEvent({Key? key});
 
   @override
-  State<crudevents> createState() => _crudeventsState();
+  ConsumerState<CrudEvent> createState() => _CrudEventState();
 }
 
-class _crudeventsState extends State<crudevents> {
+class _CrudEventState extends ConsumerState<CrudEvent> {
   @override
   Widget build(BuildContext context) {
+    final EventsData = ref.watch(eventsDataProvider);
     return Scaffold(
       appBar: AppBar(
-        title: Text('CRUD EVENTS'),
+        title: const Text('EgyMania Events'),
+        backgroundColor: Color.fromARGB(255, 28, 128, 150),
       ),
+      body: EventsData.when(
+          data: (value) => SafeArea(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                          itemCount: 2,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10.0, top: 10.0),
+                              child: Card(
+                                margin: EdgeInsets.zero,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            'Events Name: ${value.get('name')}',
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      ListView.builder(
+                                          shrinkWrap: true,
+                                          physics:
+                                              const NeverScrollableScrollPhysics(),
+                                          itemCount: 1,
+                                          itemBuilder: (BuildContext context,
+                                              int index) {
+                                            return Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 10.0),
+                                              child: Row(
+                                                children: [
+                                                  SizedBox(
+                                                    height: 50,
+                                                    width: 50,
+                                                    child: Image.asset(
+                                                      'assets/Egypt.jpg',
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 10,
+                                                  ),
+                                                  Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      const SizedBox(
+                                                        height: 10,
+                                                      ),
+                                                      SizedBox(
+                                                        width: 285,
+                                                        child: Text(
+                                                          '${value.get('about')}',
+                                                          style:
+                                                              const TextStyle(
+                                                            fontSize: 14,
+                                                          ),
+                                                          overflow:
+                                                              TextOverflow.clip,
+                                                          maxLines: 5,
+                                                        ),
+                                                      )
+                                                    ],
+                                                  )
+                                                ],
+                                              ),
+                                            );
+                                          }),
+                                      const SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Column(
+                                            children: [
+                                              // ignore: prefer_const_constructors
+                                              Text(
+                                                'Location:',
+                                                style: const TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                '${value.get('location')}',
+                                                style: const TextStyle(
+                                                  fontSize: 13,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          Column(
+                                            children: [
+                                              // ignore: prefer_const_constructors
+                                              Text(
+                                                'Price:',
+                                                style: const TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                '${value.get('price')}',
+                                                style: const TextStyle(
+                                                  fontSize: 13,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                      const SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          ElevatedButton(
+                                              onPressed: () {},
+                                              style: ElevatedButton.styleFrom(
+                                                  primary: Color.fromARGB(
+                                                      255, 28, 128, 150),
+                                                  minimumSize: Size(150, 40)),
+                                              child: Text(
+                                                "EDIT",
+                                                style: const TextStyle(
+                                                  fontSize: 12,
+                                                ),
+                                              )),
+                                          ElevatedButton(
+                                              onPressed: () {},
+                                              style: ElevatedButton.styleFrom(
+                                                  primary: Color.fromARGB(
+                                                      255, 28, 128, 150),
+                                                  minimumSize: Size(150, 40)),
+                                              child: Text(
+                                                "Delete",
+                                                style: const TextStyle(
+                                                  fontSize: 12,
+                                                ),
+                                              )),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
+                    )
+                  ],
+                ),
+              ),
+          error: (Object error, StackTrace stackTrace) {},
+          loading: () {}),
     );
   }
 }

701228641e4a2a78000e59ea0a55dd4316dff6f0
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-15 01:17:17 +0200

    edit details

---
+++ mobileapplication/lib/screens/edit_place.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EditPlace extends StatefulWidget {
+  const EditPlace({super.key});
+
+  @override
+  State<EditPlace> createState() => _EditPlaceState();
+}
+
+class _EditPlaceState extends State<EditPlace> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomSheet: Container(
+        
+      ),
+    );
+  }
+}

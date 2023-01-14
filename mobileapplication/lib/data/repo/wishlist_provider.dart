38f73989c0a6722633c8ccc25f069ea4360ad715
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-01-15 01:45:53 +0200

    creating new provider for the wishlist

---
+++ mobileapplication/lib/data/repo/wishlist_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../model/events_model.dart';
+import '../../model/whishlist_model.dart';
+
+//provider byklm almodel
+Future wishlistData = WishList().getWishListData();
+final wishlistDataProviderRepository =
+    StateProvider<Future>((ref) => wishlistData);
+//gets the data from the model
+
+final wishlistDataProvider = FutureProvider(
+  //bagyb beh aldata,byt3amel my al-ui
+  //ui byklem provider
+  (ref) async {
+    return ref.watch(wishlistDataProviderRepository);
+    //listens to the data
+  },
+);
+
+
+
+
+
+// import 'dart:core';
+// import 'package:flutter/material.dart';
+
+// class FavoriteProvider extends ChangeNotifier {
+//   List<String> _places = [];
+//   List<String> get places => _places;
+
+//   void toggleFavorite(String places) {
+//     final isExist = _places.contains(places);
+//     if (isExist) {
+//       _places.remove(places);
+//     } else {
+//       _places.add(places);
+//     }
+//     notifyListeners();
+//   }
+
+//   void clearFavorite() {
+//     _places = [];
+//     notifyListeners();
+//   }
+// }

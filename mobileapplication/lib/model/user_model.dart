f6a71cf1e78dec7de5cba587f201d3b62c0fa9a1
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-19 15:25:01 +0200

    AddModelAttrbuites5

--- mobileapplication/lib/model/user_model.dart
+++ mobileapplication/lib/model/user_model.dart
@@ -1,5 +1,44 @@
 import 'package:mobileapplication/data/dataApp/user_data.dart';
 
+import 'dart:convert';
+import 'dart:core';
+
+class UserModel {
+  String name;
+  String email;
+  String password;
+  String about;
+
+  UserModel(
+      {required this.name,
+      required this.about,
+      required this.email,
+      required this.password});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'about': about,
+      'password': password,
+      'email': email,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'] as String,
+      about: map['about'] as String,
+      password: map['password'] as String,
+      email: map['email'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+
 class UserPreferences {
   static const myUser = User(
     imagePath: 'https://cdn-icons-png.flaticon.com/512/146/146007.png',

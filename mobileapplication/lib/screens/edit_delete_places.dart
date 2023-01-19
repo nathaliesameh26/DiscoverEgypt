58050a14e046339dd34d8d6f2266e026c3880f04
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-19 14:24:37 +0200

    editImport6

--- mobileapplication/lib/screens/edit_delete_places.dart
+++ mobileapplication/lib/screens/edit_delete_places.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobileapplication/data/dataApp/place_data.dart';
 import 'package:mobileapplication/model/place_model.dart';
 import '../data/repo/places_provider.dart';
 

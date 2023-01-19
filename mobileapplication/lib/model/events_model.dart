f9308348f8cfeeb9970a8c2aff99b1148802fc98
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-19 04:37:11 +0200

    EVENTmodel

--- mobileapplication/lib/model/events_model.dart
+++ mobileapplication/lib/model/events_model.dart
@@ -23,9 +23,48 @@ class EventsData {
   //       .get();
   //   return documentSnapshot;
   // }
-}
+
+  Future<void> updateEventDetails(
+    String name,
+    String about,
+    String city,
+    String location,
+    String price,
+    String StartDate,
+    String EndDate,
+    String openingTime,
+    String closingTime,
+  ) async {
+    Map<String, dynamic> toMap() {
+      return {
+        'name': name,
+        'about': about,
+        'price': price,
+        'location': location,
+        'city': city,
+        'startDate': StartDate,
+        'endDate': EndDate,
+        'openingtime': openingTime,
+        'closingtime': closingTime,
+      };
+    }
+
+    await FirebaseFirestore.instance
+        .collection('events')
+        .where("name", isEqualTo: name)
+        .get()
+        .then((querySnapshot) {
+      for (var result in querySnapshot.docs) {
+        FirebaseFirestore.instance
+            .collection('events')
+            .doc(result.id)
+            .update(toMap());
+      }
+    });
+  }
 
 //-------------------------------Adding New Event to firebase --------------------------//
+}
 
 Future EventAdded(
   String name,

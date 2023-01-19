7145d6523abc2e4a4ae32b7a1245987b984c362e
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-19 04:10:51 +0200

    UPdateUI

--- mobileapplication/lib/screens/edit_place.dart
+++ mobileapplication/lib/screens/edit_place.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'package:mobileapplication/data/repo/places_provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:provider/provider.dart';
+import 'package:mobileapplication/model/place_model.dart';
 
 class EditPlacePage extends StatefulWidget {
   @override
@@ -12,38 +9,43 @@ class EditPlacePage extends StatefulWidget {
 
 class _EditPlacePageState extends State<EditPlacePage> {
   final _formKey = GlobalKey<FormState>();
-  // late TextEditingController nameController ;
-  // late TextEditingController aboutController;
-  // late TextEditingController priceController;
-  // late TextEditingController cityController;
-  // late TextEditingController locationController;
-  // late TextEditingController openingTimeController;
-  // late TextEditingController closingTimeController;
+  final Place_Data = PlacesData();
 
-  TextEditingController nameController = TextEditingController();
-  TextEditingController aboutController = TextEditingController();
-  TextEditingController priceController = TextEditingController();
-  TextEditingController cityController = TextEditingController();
-  TextEditingController locationController = TextEditingController();
-  TextEditingController openingTimeController = TextEditingController();
-  TextEditingController closingTimeController = TextEditingController();
+  late TextEditingController nameController;
+  late TextEditingController aboutController;
+  late TextEditingController priceController;
+  late TextEditingController cityController;
+  late TextEditingController locationController;
+  late TextEditingController openingTimeController;
+  late TextEditingController closingTimeController;
+
+  // TextEditingController nameController = TextEditingController();
+  // TextEditingController aboutController = TextEditingController();
+  // TextEditingController priceController = TextEditingController();
+  // TextEditingController cityController = TextEditingController();
+  // TextEditingController locationController = TextEditingController();
+  // TextEditingController openingTimeController = TextEditingController();
+  // TextEditingController closingTimeController = TextEditingController();
+  
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   final placeData = ref.read(placesDataProvider).value;
+  //   aboutController = TextEditingController(text: placeData.get('name'));
+  //   priceController =
+  //       TextEditingController(text: placeData.get('price').toString());
+  //   cityController = TextEditingController(text: placeData.get('city'));
+  //   locationController = TextEditingController(text: placeData.get('location'));
+  //   closingTimeController =
+  //       TextEditingController(text: placeData.get('openingtime'));
+  //   closingTimeController =
+  //       TextEditingController(text: placeData.get('closingtime'));
+  // }
+
   @override
   Widget build(BuildContext context) {
-    //   void initState() {
-    //   super.initState();
-    //   final placeData = ref.read(placesDataProvider).value;
-    //   aboutController = TextEditingController(text: placeData.get('name'));
-    //   priceController =
-    //       TextEditingController(text: placeData.get('price').toString());
-    //   cityController = TextEditingController(text: placeData.get('city'));
-    //   locationController = TextEditingController(text: placeData.get('location'));
-    //   closingTimeController =
-    //       TextEditingController(text: placeData.get('openingtime'));
-    //   closingTimeController =
-    //       TextEditingController(text: placeData.get('closingtime'));
-    // }
     return Scaffold(
-         appBar: AppBar(
+        appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 103, 58, 209),
           elevation: 0,
           title: Text(
@@ -111,12 +113,6 @@ class _EditPlacePageState extends State<EditPlacePage> {
                           width: 10,
                           color: Colors.black12,
                         ))),
-                    // validator: (value) {
-                    //   if (value!.isEmpty) {
-                    //     return 'Please enter a location';
-                    //   }
-                    //   return null;
-                    // },
                   ),
                   SizedBox(height: 20),
                   TextFormField(
@@ -128,12 +124,6 @@ class _EditPlacePageState extends State<EditPlacePage> {
                           width: 10,
                           color: Colors.black12,
                         ))),
-                    // validator: (value) {
-                    //   if (value!.isEmpty) {
-                    //     return 'Please enter a city';
-                    //   }
-                    //   return null;
-                    // },
                   ),
                   SizedBox(height: 20),
                   TextFormField(
@@ -145,12 +135,6 @@ class _EditPlacePageState extends State<EditPlacePage> {
                           width: 10,
                           color: Colors.black12,
                         ))),
-                    // validator: (value) {
-                    //   if (value!.isEmpty) {
-                    //     return 'Please enter an opening time';
-                    //   }
-                    //   return null;
-                    // },
                   ),
                   SizedBox(height: 20),
                   TextFormField(
@@ -162,17 +146,41 @@ class _EditPlacePageState extends State<EditPlacePage> {
                           width: 10,
                           color: Colors.black12,
                         ))),
-                    // validator: (value) {
-                    //   if (value!.isEmpty) {
-                    //     return 'Please enter a closing time';
-                    //   }
-                    //   return null;
-                    // },
                   ),
                   Padding(
                     padding: EdgeInsets.symmetric(vertical: 16.0),
                     child: ElevatedButton(
                       onPressed: () {
+
+                        
+    if (_formKey.currentState!.validate()) {
+       _formKey.currentState!.save();
+      Place_Data.updatePlaceDetails(       
+                           nameController.text,
+                          aboutController.text,
+                   priceController.text,
+                 cityController.text,
+  locationController.text,
+ openingTimeController.text,
+closingTimeController.text
+);
+      Navigator.pop(context);
+    }
+  
+//                         Place_Data.UpdatePlace(
+//                           nameController.text,
+//                           aboutController.text,
+//                    priceController.text,
+//                  cityController.text,
+//   locationController.text,
+//  openingTimeController.text,
+// closingTimeController.text,
+
+//                         ).then
+//                         (value)=>   ScaffoldMessenger.of(context).showSnackBar(
+//                           const SnackBar(content: Text('Successfully Added')),
+//                         );
+
                         // if (_formKey.currentState!.validate()) {
                         //   _formKey.currentState!.save();
                         //   ScaffoldMessenger.of(context).showSnackBar(
@@ -193,6 +201,14 @@ class _EditPlacePageState extends State<EditPlacePage> {
               ),
             ),
           ),
-        ));
+          
+        )
+        
+        );
+        
   }
+  
+  
 }
+
+

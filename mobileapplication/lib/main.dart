61f555639968fc592fa3238ad90ba4aed0e6bec3
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-01-20 19:58:48 +0200

    update main

--- mobileapplication/lib/main.dart
+++ mobileapplication/lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/plans',
+        initialRoute: '/',
         routes: {
           '/testScreen': (context) => testScreen(),
           '/raiting': (context) => const raiting(),

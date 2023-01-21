35d0ab049684956ac909d1cd4e395f2c8e0311eb
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-21 21:09:24 +0200

    main

--- mobileapplication/lib/main.dart
+++ mobileapplication/lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/welcome',
+        initialRoute: '/admin',
         routes: {
           '/weather': (context) => CurrentWeatherPage(),
           '/testScreen': (context) => testScreen(),

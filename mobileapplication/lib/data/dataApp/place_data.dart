985e8647f781b3c406274bc7bcda49d30f707f4a
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-21 05:42:24 +0200

    placeData

--- mobileapplication/lib/data/dataApp/place_data.dart
+++ mobileapplication/lib/data/dataApp/place_data.dart
@@ -1,10 +1,10 @@
-import 'dart:core';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 //--------------------------Connecting With Firebase ---------------------------//
 //functions start it lower cases
 class PlacesData {
-  Stream<QuerySnapshot> placeDetails() {
+  Stream<QuerySnapshot> placesDetails() {
     return FirebaseFirestore.instance.collection('places').snapshots();
   }
 
@@ -20,66 +20,50 @@ class PlacesData {
   ) async {
     final placeRef = FirebaseFirestore.instance.collection('places');
     final placeQuery = placeRef.doc(id);
+    // final eventQuery = eventRef.where('name', isEqualTo: name);
     final placeSnapshot = await placeQuery.get();
 
-    Future<void> updatePlaceDetails(
-        String name,
-        String about,
-        String city,
-        String location,
-        String price,
-        String openingTime,
-        String closingTime) async {
-      Map<String, dynamic> toMap() {
-        return {
-          'name': name,
-          'about': about,
-          'price': price,
-          'location': location,
-          'city': city,
-          'openingtime': openingTime,
-          'closingtime': closingTime,
-        };
-      }
-
-      placeSnapshot.reference.update({
-        'name': name,
-        'about': about,
-        'price': price,
-        'location': location,
-        'city': city,
-        'openingTime': openingTime,
-        'closingTime': closingTime,
-      });
-    }
-
-    Future deletePlace({required String id}) async {
-      FirebaseFirestore.instance.collection("places").doc(id).delete();
-    }
-
-    Future PlaceAdded({
-      required String name,
-      required String about,
-      required String city,
-      required String price,
-      required String location,
-      required String openingTime,
-      required String closingTime,
-    }) async {
-      //create a document to get its ID
-      final newDocument = FirebaseFirestore.instance.collection('places').doc();
-      await FirebaseFirestore.instance
-          .collection('places')
-          .doc(newDocument.id)
-          .set({
-        "name": name,
-        "about": about,
-        "city": city,
-        "price": int.parse(price),
-        "location": location,
-        "openingTime": openingTime,
-        "closingTime": closingTime,
-      });
-    }
+    placeSnapshot.reference.update({
+      'name': name,
+      'about': about,
+      'price': price,
+      'location': location,
+      'city': city,
+      'openingTime': openingTime,
+      'closingTime': closingTime,
+    });
   }
+
+  Future deletePlace({required String id}) async {
+    FirebaseFirestore.instance
+        .collection("places")
+        .doc(id)
+        .delete();
+  }
+
+
+Future placeAdded({
+  required String name,
+  required String about,
+  required String city,
+  required String price,
+  required String location,
+  required String openingTime,
+  required String closingTime,
+}) async {
+  //create a document to get its ID
+  final newDocument = FirebaseFirestore.instance.collection('places').doc();
+  await FirebaseFirestore.instance
+      .collection('places')
+      .doc(newDocument.id)
+      .set({
+    "name": name,
+    "about": about,
+    "city": city,
+    "price": int.parse(price),
+    "location": location,
+    "openingTime": openingTime,
+    "closingTime": closingTime,
+  });
+}
 }

5da007e65be0b7cda05d2d47ca1b2fd03da35e71
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-21 05:43:10 +0200

    provider

--- mobileapplication/lib/data/repo/places_provider.dart
+++ mobileapplication/lib/data/repo/places_provider.dart
@@ -1,16 +1,17 @@
-import 'package:csc_picker/model/select_status_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobileapplication/data/dataApp/place_data.dart';
+import 'package:mobileapplication/data/repo/place_provider_test.dart';
+
+//---------------------EventsProvider------------------------//
 
 //provider byklm almodel
-Stream placesData = PlacesData().placeDetails();
-final placessDataProviderRepository = StateProvider<Stream>((ref) => placesData);
+Stream evensData = PlacesData().placesDetails();
+final placesDataProviderRepository = StateProvider<Stream>((ref) => evensData);
 //gets the data from the model
 
 final placesDataProvider = StreamProvider((
   //bagyb beh aldata,byt3amel my al-ui
   //ui byklem provider
-  (ref) => ref.watch(placessDataProviderRepository))
+  (ref) => ref.watch(placesDataProviderRepository))
     //listens to the data
 );
-

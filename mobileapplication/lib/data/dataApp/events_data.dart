e8c7e28a693866f5b74a36c519d7d85e53d654d6
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-21 23:30:58 +0200

    FIAL EVENTATA

--- mobileapplication/lib/data/dataApp/events_data.dart
+++ mobileapplication/lib/data/dataApp/events_data.dart
@@ -10,15 +10,16 @@ class EventsData {
 
   Future updateEventDetails(
     String id,
-    String name,
     String about,
     String city,
-    String price,
-    String location,
-    String openingTime,
     String closingTime,
-    String startdate,
     String enddate,
+    String location,
+    String name,
+    String openingTime,
+    String plannerName,
+    String price,
+    String startdate,
   ) async {
     final eventRef = FirebaseFirestore.instance.collection('events');
     final eventQuery = eventRef.doc(id);
@@ -26,52 +27,82 @@ class EventsData {
     final eventSnapshot = await eventQuery.get();
 
     eventSnapshot.reference.update({
-      'name': name,
       'about': about,
-      'price': price,
-      'location': location,
       'city': city,
-      'startdate': startdate,
-      'enddate': enddate,
-      'openingTime': openingTime,
       'closingTime': closingTime,
+      'enddate': enddate,
+      'location': location,
+      'name': name,
+      'openingTime': openingTime,
+      'plannerName': plannerName,
+      'price': price,
+      'startdate': startdate,
     });
   }
 
   Future deleteEvent({required String id}) async {
-    FirebaseFirestore.instance
-        .collection("events")
-        .doc(id)
-        .delete();
+    FirebaseFirestore.instance.collection("events").doc(id).delete();
   }
 
+  Future EventAdded({
+    required String name,
+    required String plannerName,
+    required String about,
+    required String city,
+    required String price,
+    required String startdate,
+    required String enddate,
+    required String location,
+    required String openingTime,
+    required String closingTime,
+  }) async {
+    //create a document to get its ID
+    final newDocument = FirebaseFirestore.instance.collection('events').doc();
+    await FirebaseFirestore.instance
+        .collection('events')
+        .doc(newDocument.id)
+        .set({
+      "name": name,
+      "about": about,
+      "plannerName": plannerName,
+      "city": city,
+      "price": int.parse(price),
+      "location": location,
+      "startdate": startdate,
+      "enddate": enddate,
+      "openingTime": openingTime,
+      "closingTime": closingTime,
+    });
+  }
 
-Future EventAdded({
-  required String name,
-  required String about,
-  required String city,
-  required String price,
-  required String startdate,
-  required String enddate,
-  required String location,
-  required String openingTime,
-  required String closingTime,
-}) async {
-  //create a document to get its ID
-  final newDocument = FirebaseFirestore.instance.collection('events').doc();
-  await FirebaseFirestore.instance
-      .collection('events')
-      .doc(newDocument.id)
-      .set({
-    "name": name,
-    "about": about,
-    "city": city,
-    "price": int.parse(price),
-    "location": location,
-    "startdate": startdate,
-    "enddate": enddate,
-    "openingTime": openingTime,
-    "closingTime": closingTime,
-  });
-}
+  Future EventAddedPlanner({
+    required String name,
+    required String plannerName,
+    required String about,
+    required String city,
+    required String price,
+    required String startdate,
+    required String enddate,
+    required String location,
+    required String openingTime,
+    required String closingTime,
+  }) async {
+    //create a document to get its ID
+    final newDocument = FirebaseFirestore.instance.collection('pending').doc();
+    await FirebaseFirestore.instance
+        .collection('pending')
+        .doc(newDocument.id)
+        .set({
+      "name": name,
+      "about": about,
+      "plannerName": plannerName,
+      "city": city,
+      "price": int.parse(price),
+      "location": location,
+      "startdate": startdate,
+      "enddate": enddate,
+      "openingTime": openingTime,
+      "closingTime": closingTime,
+    });
+  }
 }

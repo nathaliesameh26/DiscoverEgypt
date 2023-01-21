2c80e729a1f69745066b0e5a2f2b838a477e102c
Remon74 <remon1908077@miuegypt.edu.eg>
2023-01-21 18:40:20 +0200

    Update main.dart

--- mobileapplication/lib/main.dart
+++ mobileapplication/lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/EditProfilePage',
+        initialRoute: '/login',
         routes: {
           '/weather': (context) => CurrentWeatherPage(),
           '/testScreen': (context) => testScreen(),

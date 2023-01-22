70c432a45e94107593666a143f65bc0164bac468
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-01-22 04:00:31 +0200

    update nabar

--- mobileapplication/lib/widget/nav_bar_test.dart
+++ mobileapplication/lib/widget/nav_bar_test.dart
@@ -85,3 +85,110 @@
 //              );
 //          }
 //      }
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../screens/homeScreen.dart';
+import '../screens/myplan_screen.dart';
+
+class narBar extends StatefulWidget {
+  const narBar({super.key});
+
+  @override
+  State<narBar> createState() => _navbarstate();
+}
+
+class _navbarstate extends State<narBar> {
+  int index = 0;
+  final screens = [
+    const HomeScreenn(),
+    MyPlansScreen(),
+    MyPlansScreen(),
+    MyPlansScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: NavigationBarTheme(
+            data: NavigationBarThemeData(
+              indicatorColor: Colors.blue.shade100,
+              labelTextStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+            ),
+            child: NavigationBar(
+              height: 60,
+              backgroundColor: Colors.grey,
+              selectedIndex: index,
+              onDestinationSelected: (index) =>
+                  setState(() => this.index = index),
+              destinations: [
+                // screens[index],
+                // ignore: prefer_const_literals_to_create_immutables
+                NavigationBar(destinations: [
+                  const NavigationDestination(
+                      icon: Icon(Icons.home), label: 'Home'),
+                  const NavigationDestination(
+                      icon: Icon(Icons.favorite_border), label: 'Wishlist'),
+                  const NavigationDestination(
+                      icon: Icon(Icons.book), label: 'Plans'),
+                  const NavigationDestination(
+                      icon: Icon(Icons.person), label: 'Profile')
+                ])
+              ],
+            )));
+  }
+}
+
+
+// bottomNavigationBar: NavigationBarTheme(
+      //     data: NavigationBarThemeData(
+      //       indicatorColor: Colors.blue.shade100,
+      //       labelTextStyle: MaterialStateProperty.all(
+      //           TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+      //     ),
+      //     child: NavigationBar(
+      //       height: 60,
+      //       backgroundColor: Colors.grey,
+
+      //       selectedIndex: index,
+      //       onDestinationSelected: (index) =>
+      //           setState(() => this.index = index),
+      //       destinations: [
+      //         // screens[index],
+      //         // ignore: prefer_const_literals_to_create_immutables
+      //         NavigationBar(destinations: [
+      //           const NavigationDestination(
+      //               icon: Icon(Icons.home), label: 'Home'),
+      //           const NavigationDestination(
+      //               icon: Icon(Icons.favorite_border), label: 'Wishlist'),
+      //           const NavigationDestination(
+      //               icon: Icon(Icons.book), label: 'Plans'),
+      //           const NavigationDestination(
+      //               icon: Icon(Icons.person), label: 'Profile')
+      //         ])
+      //       ],
+      //       // bottomNavigationBar: const GNav(
+      //       //   tabBackgroundColor: Colors.grey,
+      //       //   tabs: [
+      //       //     GButton(
+      //       //       icon: Icons.home,
+      //       //       text: 'Home',
+      //       //       //onPressed: () {},
+      //       //     ),
+      //       //     GButton(
+      //       //       icon: Icons.favorite_border,
+      //       //       text: 'Favorites',
+      //       //     ),
+      //       //     GButton(
+      //       //       icon: Icons.book,
+      //       //       text: 'Plans',
+      //       //     ),
+      //       //     GButton(
+      //       //       icon: Icons.person,
+      //       //       text: 'Profile',
+      //       //     ),
+      //       //   ],
+      //       // ),
+      //     )),

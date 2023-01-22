054ee5286dbd5dae4d6a504bcc735359c1e06ef8
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-01-22 05:33:12 +0200

    pend

--- mobileapplication/lib/data/dataApp/pending_data.dart
+++ mobileapplication/lib/data/dataApp/pending_data.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 final pendingEvent = FirebaseAuth.instance.currentUser!;
 String pendingEID = pendingEvent.uid;
@@ -8,54 +7,44 @@ String pendingEID = pendingEvent.uid;
 class PendingData {
   // ignore: non_constant_identifier_names
 
-  Future PendingDetails() async {
-    final QuerySnapshot pendingEvent =
-        await FirebaseFirestore.instance.collection('pending').get();
-        
-    return pendingEvent;
+  Stream<QuerySnapshot> pendingDetails() {
+    return FirebaseFirestore.instance.collection('pending').snapshots();
   }
 
-  // Future<Object> PendingDetails() async {
-  //   final pendingEvent = FirebaseAuth.instance.currentUser!;
-  //   String pendingEID = pendingEvent.uid;
-  //   final DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
-  //       .collection('pending')
-  //       .doc('j4YSu0J2ZGfaGKDMbswb')
-  //       .get();
-  //   return documentSnapshot;
-  // }
+  Future deletePendingEvent({required String id}) async {
+    FirebaseFirestore.instance.collection("pending").doc(id).delete();
+  }
+
+  Future pendingEventAdded({
+    required String name,
+    required String plannerName,
+    required String about,
+    required String city,
+    required String price,
+    required String startdate,
+    required String enddate,
+    required String location,
+    required String openingTime,
+    required String closingTime,
+  }) async {
+    //create a document to get its ID
+    final newDocument = FirebaseFirestore.instance.collection('events').doc();
+    await FirebaseFirestore.instance
+        .collection('events')
+        .doc(newDocument.id)
+        .set({
+      "name": name,
+      "about": about,
+      "plannerName": plannerName,
+      "city": city,
+      "price": int.parse(price),
+      "location": location,
+      "startdate": startdate,
+      "enddate": enddate,
+      "openingTime": openingTime,
+      "closingTime": closingTime,
+    });
+  }
+
+
 }
-
-// class PendingModel {
-//   String? name;
-//   String? location;
-//   String? about;
-//   int? price;
-
-//   PendingModel(this.name, this.location, this.about, this.price);
-// }
-
-
-// Future addEvent({
-//   required String name,
-//   required String about,
-//   required String location,
-//   required int price,
-//   required int rating,
-//   required String openingtime,
-//   required String closingtime,
-// }) async {
-//   await FirebaseFirestore.instance.collection('events').doc(pendingEID).set({
-//     "name": name,
-//     "about": about,
-//     "location": location,
-//     "price": price,
-//     "rating": rating,
-//     "openingtime": openingtime,
-//     "closingtime": closingtime,
-//   });
-
-//   print('New event is added');
-//   final prefs = await SharedPreferences.getInstance();
-//   await prefs.setString('PLACE_ID', pendingEID);
-// }
